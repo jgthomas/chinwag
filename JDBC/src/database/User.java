0bda325dfaa862b817f1dea4766ce4efde95b884
Aidan Wilkinson <aidan@aidan-XPS-13-9370>
2019-03-05 13:39:44 +0000

    database stuff

---
+++ JDBC/src/database/User.java
@@ -0,0 +1,25 @@
+package database;
+
+public class User {
+	private String username, password;
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String toString() {
+		return "\n[username=" + username + ", password=" + password + "]";
+	}
+	
+	
+}
