0a57d5825063f9fe3b857d227d80b264a2040ece
Ed Wormington <wormingtone@gmail.com>
2019-03-06 19:26:29 +0000

    Some basic poker components

---
+++ src/poker/Deck.java
@@ -0,0 +1,15 @@
+package poker;
+
+public class Deck {
+	private String order; //Make this something else
+	
+	public Deck() {
+		//generate
+	}
+	
+	public void shuffle() {
+		//shuffles
+	}
+	
+
+}
