ec570631abf8b1428abc65e433e3ac71623c4aaa
James Thomas <codeactor@protonmail.com>
2019-03-06 17:43:33 +0000

    debugging

--- src/server/LoginCommand.java
+++ src/server/LoginCommand.java
@@ -11,13 +11,13 @@ class LoginCommand extends Command {
 
 	@Override
 	public void execute(MessageBox messageBox) {
-		String username = messageBox.get(Data.USERNAME);
+		String username = messageBox.get(Data.USER_NAME);
 		String password = messageBox.get(Data.PASSWORD);
 		if (Database.isValidUser(username, password)){
 			MessageBox mb = new MessageBox(Action.SERVER_MESSAGE);
 			mb.add(Data.MESSAGE, "Login successful!");
 			getMessageSender().sendMessage(mb);
-			setUserName(messageBox.get(Data.USERNAME));
+			setUserName(messageBox.get(Data.USER_NAME));
 			registerSender();
 			//getMessageSender().sendMessage(new MessageBox(Action.CHAT, "Hello", "Hello!"));
 		}
