00f51e44a5e86aa335a7b43c81a5b935c3ec3412
Ed Wormington <wormingtone@gmail.com>
2019-03-06 13:37:20 +0000

    Update to my own class

--- ClientEd/ReactThread.java
+++ ClientEd/ReactThread.java
@@ -1,17 +1,24 @@
 package ClientEd;
 
+import server.Action;
+import server.MessageBox;
+
 public class ReactThread implements Runnable {
+	private ClientDraft client;
+	private MessageBox message;
 	
 	ReactThread(ClientDraft client, MessageBox message) {
-		if (!Action.isValidCommand(message.getCommand())) {
-			
+			this.client = client;
+			this.message = message;
 		}
 		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		if (!Action.isValidCommand(message.getCommand().getToken())) {
+			//Throw exception?
+			//Do nothing?
 		
 	}
 
