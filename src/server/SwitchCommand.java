29c32612ddfa8b431bb5111d4221f0ca2df247a5
James Thomas <codeactor@protonmail.com>
2019-03-06 16:05:06 +0000

    comment out old code

--- src/server/SwitchCommand.java
+++ src/server/SwitchCommand.java
@@ -1,14 +1,14 @@
-package server;
-
-
-class SwitchCommand extends Command {
-
-        SwitchCommand(ConnectionTracker connectionTracker) {
-                super(connectionTracker);
-        }
-
-        @Override
-        public void execute(MessageBox messageBox) {
-                getConnectionTracker().setCurrentSessionName(messageBox.getData());
-        }
-}
+//package server;
+//
+//
+//class SwitchCommand extends Command {
+//
+//        SwitchCommand(ConnectionTracker connectionTracker) {
+//                super(connectionTracker);
+//        }
+//
+//        @Override
+//        public void execute(MessageBox messageBox) {
+//                getConnectionTracker().setCurrentSessionName(messageBox.getData());
+//        }
+//}
--- src/server/UsersCommand.java
+++ src/server/UsersCommand.java
@@ -1,22 +1,22 @@
-package server;
-
-import java.util.List;
-
-
-class UsersCommand extends Command {
-
-        UsersCommand(ConnectionTracker connectionTracker) {
-                super(connectionTracker);
-        }
-
-        @Override
-        public void execute(MessageBox messageBox) {
-                getMessageSender().sendMessage(new MessageBox(getNames()));
-        }
-
-        private String getNames() {
-                List<String> names =
-                        getConnectionTracker().getCurrentSession().allUserNames();
-                return names.toString();
-        }
-}
+//package server;
+//
+//import java.util.List;
+//
+//
+//class UsersCommand extends Command {
+//
+//        UsersCommand(ConnectionTracker connectionTracker) {
+//                super(connectionTracker);
+//        }
+//
+//        @Override
+//        public void execute(MessageBox messageBox) {
+//                getMessageSender().sendMessage(new MessageBox(getNames()));
+//        }
+//
+//        private String getNames() {
+//                List<String> names =
+//                        getConnectionTracker().getCurrentSession().allUserNames();
+//                return names.toString();
+//        }
+//}
