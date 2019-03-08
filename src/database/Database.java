42454595aa36bdbc664a30c1924ec11491578a1c
Aidan Wilkinson <aidan@aidan-XPS-13-9370>
2019-03-08 13:08:03 +0000

    .

--- src/database/Database.java
+++ src/database/Database.java
@@ -10,13 +10,14 @@ public class Database {
     private static String url = "jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk/group22";
     private static String username = "group22";
     private static String password = "group22";
-    private static Connection connection;
+    private static Connection connection = makeConnection();
 
-    public static void makeConnection(){
+    public static Connection makeConnection(){
         try {
-            connection = DriverManager.getConnection(url, username, password);
+            return DriverManager.getConnection(url, username, password);
         } catch (SQLException e){
             e.printStackTrace();
+            return null;
         }
     }
 
@@ -56,6 +57,20 @@ public class Database {
         }
     }
 
+    /* Planning to keep attempts counter on server because this is temporary.
+     * If later we decide that we want to store the attempts counter on the 
+     * database then we can reinstate the following method:
+     */
+//    public static synchronized void makeAttemptsIncrement(String username, int i){
+//        try (PreparedStatement statement = connection.prepareStatement(
+//        		"UPDATE users SET attempts = ? WHERE username = ?")){
+//            statement.setInt(1, i + 1);
+//            statement.setString(2, username);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     public static synchronized void insertNewUser(String username, String password){
         try (PreparedStatement statement = connection.prepareStatement(
         		"INSERT INTO users (username, password, attempts, locked_time) VALUES (?, ?, 0, null)")){
@@ -91,14 +106,4 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
-
-	public static void addUserToChat(String chatname, String username){
-	    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO chatsession (chatname, username) VALUES (?, ?)")){
-	        statement.setString(1, chatname);
-	        statement.setString(2, username);
-	        statement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
