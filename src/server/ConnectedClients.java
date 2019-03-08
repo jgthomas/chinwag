206ec410d550a50403112f975813145abffd7702
James Thomas <codeactor@protonmail.com>
2019-03-08 13:14:22 +0000

    new connected clients class

---
+++ src/server/ConnectedClients.java
@@ -0,0 +1,27 @@
+package server;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ConnectedClients {
+    private final ConcurrentMap<String, MessageHandler> clientsByID;
+    private final ConcurrentMap<String, MessageHandler> clientsByUserName;
+
+    ConnectedClients() {
+        clientsByID = new ConcurrentHashMap<>();
+        clientsByUserName = new ConcurrentHashMap<>();
+     }
+
+     void addClientByID(String id, MessageHandler messageHandler) {
+        clientsByID.put(id, messageHandler);
+     }
+
+     void addClientByUserName(String id, String userName) {
+        MessageHandler mh = clientsByID.get(id);
+        clientsByUserName.put(userName, mh);
+     }
+
+     MessageHandler getClientByUserName(String userName) {
+        return clientsByUserName.get(userName);
+     }
+}
