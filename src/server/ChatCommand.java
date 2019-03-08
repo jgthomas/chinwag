0b27984aec10af3b3d9831ea8e74b90865469984
James Thomas <codeactor@protonmail.com>
2019-03-08 07:15:34 +0000

    edit todo

--- src/server/ChatCommand.java
+++ src/server/ChatCommand.java
@@ -14,7 +14,7 @@ import protocol.MessageBox;
  * TODO: specify the chat session to which the message should be posted
  *
  * Currently it is sent to the 'activeSession' as tracked on the server, we can
- * either have the client update this when active session changes, i.e. when GUI window
+ * either have the client update this when active session changes, e.g. when GUI window
  * focus alters, and keep the current system. Or, we can have the client specify the
  * current chat session in the MessageBox directly, which would add a Data.CHAT_NAME
  * to the contract.
