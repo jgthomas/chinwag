5eeb70d02b133a8c5f046f716f3fed4120246c69
James Thomas <codeactor@protonmail.com>
2019-03-08 07:46:56 +0000

    remove id from user class

--- src/server/User.java
+++ src/server/User.java
@@ -4,20 +4,11 @@ import java.net.*;
 
 
 class User {
-
-        // userName is username
         private String userName;
         private String screenName;
-        private String id;
-
 
         User(Socket clientSocket) {
                 this.userName = "you";
-                this.id = setID(clientSocket);
-        }
-
-        String id() {
-                return id;
         }
 
         void setUserName(String userName) {
@@ -26,9 +17,5 @@ class User {
 
         String getUserName() {
                 return userName;
-        }
-
-        private String setID(Socket clientSocket){
-                return clientSocket.getInetAddress().getHostAddress() + " " + clientSocket.getPort();
         }
 }
