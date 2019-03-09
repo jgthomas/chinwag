7c5dda67a28b6bc6110e938d76dda439fc7abf62
James Thomas <codeactor@protonmail.com>
2019-03-09 08:01:04 +0000

    javadoc on methods

--- src/server/Command.java
+++ src/server/Command.java
@@ -16,17 +16,42 @@ abstract class Command {
                 this.connectedClients = connectedClients;
         }
 
+        /**
+         * @return the message sender object for use with this command
+         * */
         MessageSender getMessageSender() {
                 return messageSender;
         }
 
+        /**
+         * @return the session tracker object for use with this command
+         * */
         SessionTracker getSessionTracker() {
                 return sessionTracker;
         }
 
+        /**
+         * @return an object tracking all the connected clients
+         * */
         ConnectedClients getConnectedClients() {
                 return connectedClients;
         }
 
+        /**
+         * Gets an single user from those currently connected
+         *
+         * @param userName the name of the user to get
+         * @return a user's message handler object
+         * */
+        MessageHandler getUser(String userName) {
+                return connectedClients.getClientByUserName(userName);
+        }
+
+        /**
+         * Executes the command received from the client.
+         * Overridden in subclasses.
+         *
+         * @param messageBox the command from the client to perform
+         * */
         abstract void execute(MessageBox messageBox);
 }
