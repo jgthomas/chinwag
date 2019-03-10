972691fa129b92aea0a7c56647361b7b4aabc7a6
Haojie Chen <hxc889@cs.bham.ac.uk>
2019-03-10 22:31:35 +0000

    "Updated Database class and changed makeConncection method"

--- src/database/Database.java
+++ src/database/Database.java
@@ -9,14 +9,13 @@ public class Database {
     private static String url = "jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk/group22";
     private static String username = "group22";
     private static String password = "group22";
-    private static Connection connection = makeConnection();
+    private static Connection connection;
 
-    public static Connection makeConnection(){
+    public static void makeConnection(){
         try {
-            return DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e){
             e.printStackTrace();
-            return null;
         }
     }
 
