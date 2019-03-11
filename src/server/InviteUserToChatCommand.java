d4c216164023e10e61a45c16ce02aa7f274b1a81
Aidan Wilkinson <aidan@aidan-XPS-13-9370>
2019-03-11 16:12:24 +0000

    added javadoc and added chatname to invite messagebox

--- src/server/InviteUserToChatCommand.java
+++ src/server/InviteUserToChatCommand.java
@@ -4,6 +4,17 @@ import protocol.Action;
 import protocol.Data;
 import protocol.MessageBox;
 
+/**
+ * CONTRACT
+ *
+ * Action: Action.INVITE
+ *
+ * Data Required
+ * Data.CHAT_NAME - the name of the chat
+ * Data.USER_NAME - the user to invite to the chat
+ * 
+ *
+ * */
 public class InviteUserToChatCommand extends Command {
 
 	InviteUserToChatCommand(MessageSender messageSender, UserChatSessions userChatSessions,
@@ -12,6 +23,9 @@ public class InviteUserToChatCommand extends Command {
 		super(messageSender, userChatSessions, allChatSessions, connectedClients);
 	}
 
+	/**
+	 * Sends an invite to invitee to join a chat session
+	 */
 	@Override
 	void execute(MessageBox messageBox) {
 		// pull out invitee username and chatname they're invited to
@@ -21,7 +35,9 @@ public class InviteUserToChatCommand extends Command {
 		// build messagebox for invitee with invite command and username of sender
 		MessageBox inviteBox = new MessageBox(Action.INVITE);
 		inviteBox.add(Data.USER_NAME, getMessageSender().getUserName());
+		inviteBox.add(Data.CHAT_NAME, chatName);
 		
+		// if invitee is online then send invite to them
 		if(getConnectedClients().getClientByUserName(invitee) != null) {
 			getConnectedClients().getClientByUserName(invitee).getMessageSender().sendMessage(inviteBox);			
 		}
