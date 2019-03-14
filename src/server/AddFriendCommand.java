29e20c7a5f31c893434694317fd1caf1ff108900
Aidan Wilkinson <ajp-wilkinson@outlook.com>
2019-03-14 21:42:46 +0000

    adds friend to db if/else block

--- src/server/AddFriendCommand.java
+++ src/server/AddFriendCommand.java
@@ -1,7 +1,20 @@
 package server;
 
+import database.Database;
+import protocol.Action;
+import protocol.Data;
 import protocol.MessageBox;
 
+
+/**
+ * CONTRACT
+ *
+ * Action: Action.ADD_FRIEND
+ *
+ * Data Required
+ * Data.USER_NAME - username of friend
+ *
+ * */
 public class AddFriendCommand extends Command {
 
 	AddFriendCommand(MessageSender messageSender, UserChatSessions userChatSessions, AllChatSessions allChatSessions,
@@ -12,7 +25,15 @@ public class AddFriendCommand extends Command {
 
 	@Override
 	void execute(MessageBox messageBox) {
-		
+		String friend = messageBox.get(Data.USER_NAME);
+		if(!Database.isFriend(getCurrentThreadUserName(), friend)) {
+			Database.insertFriend(getCurrentThreadUserName(), friend);
+			
+		} else {
+			MessageBox mb = new MessageBox(Action.SERVER_MESSAGE);
+			mb.add(Data.MESSAGE, "This user is already in your friend list");
+			getMessageSender().sendMessage(mb);
+		}
 	}
 
 }
