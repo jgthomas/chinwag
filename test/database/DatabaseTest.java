394817843b4c6ff5150d7a62b4149454534d9a54
Aidan Wilkinson <aidan@aidan-XPS-13-9370>
2019-03-19 12:41:30 +0000

    started database tests

---
+++ test/database/DatabaseTest.java
@@ -0,0 +1,30 @@
+package database;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseTest {
+    private static String url = "jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk/group22";
+    private static String username = "group22";
+    private static String password = "group22";
+	Connection connection;
+	
+	@Before
+	public void before() {
+		connection = DriverManager.getConnection(url, username, password);
+	}
+	
+	@After
+	public void after() {
+		connection.close();
+	}
+	
+	// test connection
+	@Test
+	public void test1a() {
+		
+	}
+}
