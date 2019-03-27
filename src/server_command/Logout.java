9bbaec09f26c9fe9c6ee99f2c582f4198e4632cf
ajw853 <ajw853@cca-lg04-027.cs.bham.ac.uk>
2019-03-27 17:48:04 +0000

    filled in execute method of Logout command

--- src/server_command/Logout.java
+++ src/server_command/Logout.java
@@ -1,8 +1,11 @@
 package server_command;
 
+import protocol.Action;
+import protocol.Data;
 import protocol.MessageBox;
 import server.AllChatSessions;
 import server.ConnectedClients;
+import server.MessageHandler;
 import server.MessageSender;
 import server.UserState;
 
@@ -15,9 +18,18 @@ public class Logout extends Command {
 
 	@Override
 	public void execute(MessageBox messageBox) {
+        
+		MessageBox mb = new MessageBox(Action.UPDATE_LOGGED_OUT);
+		mb.add(Data.USER_NAME, getMessageSender().getUserName());
+
+		for (MessageHandler user : getConnectedClients()) {
+			user.getMessageSender().sendMessage(mb);
+		}
 		
+		getConnectedClients().removeClientByUserName(getMessageSender().getUserName());
+		getConnectedClients().removeClientByID(getMessageSender().id());
 		
-		
+        getUserState().exitAllChats(getMessageSender());
 	}
 
 }
