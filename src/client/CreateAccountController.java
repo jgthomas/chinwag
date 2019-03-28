0699833699b549f4b569269d2eecefb23ee4eced
Ed Wormington <wormingtone@gmail.com>
2019-03-28 00:48:49 +0000

    Removed problem of overlapping error messages when signing up account

--- src/client/CreateAccountController.java
+++ src/client/CreateAccountController.java
@@ -56,6 +56,7 @@ public class CreateAccountController {
 		}
 		else {
 			usernameField.clear();
+			duplicateUsername.setVisible(false);
 			notLetters.setVisible(true);
 			usernameField.requestFocus();
 		}
@@ -83,6 +84,7 @@ public class CreateAccountController {
 	}
 	
 	public void displayDuplicateUser() {
+		notLetters.setVisible(false);
 		duplicateUsername.setVisible(true);
 	}
 	
